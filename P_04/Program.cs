77af69559b7612049d4d5a20be988ef2a4d0b45e
Sh-OV <shchegolga@yandex.ru>
2022-08-07 23:08:06 +0500

    Добавила сортировка выбором строкового массива

--- P_04/Program.cs
+++ P_04/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Урок 4. Алгоритм «Сортировка выбором»
 
 Console.Clear();
+Console.WriteLine("Вариант сортироаки выбором числового массива:");
 Console.WriteLine("Введите количество элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 // Заполнение массива
@@ -31,4 +32,37 @@ for (int i = 0; i < n - 1; i++)
 }
 Console.WriteLine();
 Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+
+Console.WriteLine("Вариант сортироаки выбором строкового массива:");
+Console.WriteLine("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+// Заполнение массива
+string[] starray = new string[m];
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("Введите строку: ");
+    starray[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", starray) + "]");
+// Сортировка
+for (int i = 0; i < m - 1; i++)
+{
+    int minIndex = i;
+    for (int j = i + 1; j < m; j++)
+    {
+        if (starray[j].Length < starray[minIndex].Length)
+            minIndex = j;
+            Console.WriteLine($"i = {i}; j = {j}; minIndex = {minIndex}");
+    }
+    string temp;
+    temp = starray[minIndex];
+    Console.WriteLine($"i = {i}; temp = {temp}; starray[i] = {starray[i]}");
+    starray[minIndex] = starray[i];
+    starray[i] = temp;
+    
+}
+Console.WriteLine();
+Console.WriteLine("Конечный массив: [" + string.Join(", ", starray) + "]");
 Console.WriteLine();
