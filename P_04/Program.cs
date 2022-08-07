5115666a321067937162701c72fc6d04c2d3ad5c
Sh-OV <shchegolga@yandex.ru>
2022-08-07 22:05:41 +0500

    Сортировка массива выбором

---
+++ P_04/Program.cs
@@ -0,0 +1,29 @@
+﻿// Урок 4. Алгоритм «Сортировка выбором»
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// Заполнение массива
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+// Сортировка
+for (int i = 0; i < n - 1; i++)
+{
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j] < array[minIndex])
+            minIndex = j;
+    }
+    int temp;
+    temp = array[minIndex];
+    array[minIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine();
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
